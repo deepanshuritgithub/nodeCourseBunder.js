7510266f26a35f169f96287cb893623051b92cdf
Deepanshu <deepanshuritgithub>
2024-07-29 15:34:03 +0530

    checks

--- app.js
+++ app.js
@@ -10,26 +10,26 @@ config({
 
 const app = express();
 
-//so yha pe hmme kuch chize dene hai otherwise hm cookies transfer nahi kr payengee , so credentials hmme mandatory hai jo hmme true krne hai 
 
 //using middlwares to handle json data and Url encoded form data 
 app.use(express.json()); 
 app.use(
     express.urlencoded({
-    extended: true,  //to make req.body.name to be treated as string instead of object if it is not provided in the request body
+        extended: true,  //to make req.body.name to be treated as string instead of object if it is not provided in the request body
     })
 );
 
 app.use(cookieParser());  //to use cookies in express app
 
 
+//so yha pe hmme kuch chize dene hai otherwise hm cookies transfer nahi kr payengee , so credentials hmme mandatory hai jo hmme true krne hai 
 app.use(
     cors({
     origin:process.env.FRONTEND_URL,
-    credentials:true,
+    credentials:true, //ye true rkhana hai otherwise nahi kr payenge send cookies ko 
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 })
-);//Ye kis Liye hai agr ye hm na de to es server se 2sri website pe hm request hi nahi kr payengee 
+);//Ye kis Liye hai agr ye hm na de to es server se 2sri website pe hm request hi nahi kr payengee , or ye jo hmne origin pass kiya hai frontend ka url bass isii website ko allow hai ye sari api use krne , otherwise koi nahi kr payegii
 
 //default route set 
 app.get("/", (req,res)=>{
